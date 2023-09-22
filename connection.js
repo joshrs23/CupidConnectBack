07e52f5b0ea6cfdabdaf92e2369c9ad311c179dd
jose salazar <joshrs23@gmail.com>
2023-09-22 14:49:59 -0400

    node is working

--- connection.js
+++ connection.js
@@ -3,14 +3,13 @@ const cors = require('cors');
 const fs = require('fs');
 const https = require('https');
 require('dotenv').config();
-require('./models/db');
 
 const app = express();
 
 
 
 
-const puerto = 8000;
-https.createServer(options, app).listen(puerto, () => {
-  console.log(`Server HTTPS is listening to the port ${puerto}`);
+const port = 8000;
+https.createServer(app).listen(port, () => {
+  console.log(`Server HTTPS is listening to the port ${port}`);
 });
